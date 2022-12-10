d2a92e4cbef1c4c53351f3dd331ef086cdb5d7e5
abhijit424515 <abhijit.amrendra.kumar@gmail.com>
2022-12-10 11:46:10 +0530

    finished dicee

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -7,7 +8,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: const Text('Dicee'),
+          title: const Center(child: Text('Dicee')),
           backgroundColor: Colors.red,
         ),
         body: const DicePage(),
@@ -16,8 +17,28 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+// class DicePage extends StatelessWidget {
+//   const DicePage({super.key});
+
+// }
+
+class DicePage extends StatefulWidget {
   const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 3;
+  int rightDiceNum = 2;
+
+  void changeDieFaces() {
+    setState(() {
+      leftDiceNum = 1 + Random().nextInt(6);
+      rightDiceNum = 1 + Random().nextInt(6);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,14 +47,14 @@ class DicePage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: TextButton(
-              onPressed: () => {},
-              child: Image.asset("images/dice1.png"),
+              onPressed: () => changeDieFaces(),
+              child: Image.asset("images/dice$leftDiceNum.png"),
             ),
           ),
           Expanded(
             child: TextButton(
-              onPressed: () => {},
-              child: Image.asset("images/dice3.png"),
+              onPressed: () => changeDieFaces(),
+              child: Image.asset("images/dice$rightDiceNum.png"),
             ),
           ),
         ],
